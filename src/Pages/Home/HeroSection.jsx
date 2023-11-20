53a30104779e2a03e5e28b639b6e7d3567d9aaf9
Kosala Dayarathne <dayarathnekosala90@gmail.com>
2023-11-20 20:41:24 +0530

    hero & navbar completed

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -5,21 +5,21 @@ const HeroSection = () => {
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hello, I,m Kosala</p>
+          <p className="section--title">Hey, I'm Kosala</p>
           <h1 className="hero--section--title">
-            <span className="hero--section--title--color">Full Stack</span>{" "}
+            <span className="hero--section-title--color">Full Stack</span>{" "}
             <br />
             Developer
           </h1>
-          <p className="hero--section--description">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            <br /> Lorem ipsum dolor sit amet consectetur adipisicing
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
           </p>
         </div>
         <button className="btn btn-primary">Get In Touch</button>
       </div>
       <div className="hero--section--img">
-        <img src="./img/hero_img.png" alt="Hero section image" />
+        <img src="./img/hero_img.png" alt="Hero Section" />
       </div>
     </section>
   );
